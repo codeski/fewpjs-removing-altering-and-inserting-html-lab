33dcf334143b1e4bedc4409c1b5bbdc61e6f8562
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-03-13 20:29:42 -0700

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector('main').remove()
+
+const newHeader = document.createElement('h1')
+
+document.body.appendChild(newHeader).setAttribute('id', 'victory')
+
+newHeader.textContent = "Todd is the champion"
+
